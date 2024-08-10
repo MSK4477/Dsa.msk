70d79be42e422419302dd331c711e4639edf279a
Saravana Kumar M <ks7997067@gmail.com>
2024-08-11 00:03:35 +0530

    search in rotated array implemented using binary search

--- techDose/assignments/binarySearch/searchInRotatedArr.cpp
+++ techDose/assignments/binarySearch/searchInRotatedArr.cpp
@@ -3,22 +3,10 @@
 using namespace std;
 
 
-
-int main () { 
-    vector<int> nums = {4,5,6,7,0,1,2};
-    int target = 0;
-
-    int foundedTarget = foundTarget(nums,target);
-
-    cout << foundedTarget << " this is the foundes Target " << endl;
-
-} 
-
 int foundTarget (vector<int>& nums, int target) { 
 
     int left = 0;
     int right = nums.size() -1;
-
     if(nums.size() ==1) { 
         if(nums[0] == target) { 
             return 0;
@@ -27,13 +15,13 @@ int foundTarget (vector<int>& nums, int target) {
         }
     }
 
-    while (left < right) 
+    while (left <= right) 
     {
         int mid = left + (right - left) /2;
 
         if(nums[mid] == target) { 
             return mid;
-        }else if(nums[left] < nums[mid]) { 
+        }else if(nums[left] <= nums[mid]) { 
 
             if(target >= nums[left] && target <= nums[mid]) { 
                 right = mid-1;
@@ -41,16 +29,26 @@ int foundTarget (vector<int>& nums, int target) {
                 left = mid +1;
             }
 
-        }else if(nums[mid] < nums[right]) { 
-           if(target >= nums[mid] && target <= nums[right])  { 
+        }else if(target >= nums[mid] && target <= nums[right])  { 
             left = mid +1;
            }else { 
             right = mid -1;
            }
-        }
     }
-    
-
+  return -1;
 }
 
+
+
+
+
+int main () { 
+    vector<int> nums = {1};
+    int target = 0;
+
+    int foundedTarget = foundTarget(nums,target);
+
+    cout << foundedTarget << " this is the founded Target " << endl;
+
+} 
 
